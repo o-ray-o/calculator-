47dc4f09476d1951cb76366f916535860b4fdaf9
o-ray-o <goh.rayfong@gmail.comr>
2022-06-14 01:02:52 +0800

    add functionalities

---
+++ main.js
@@ -0,0 +1,27 @@
+let currentNum = "";
+let previousNum = "";
+let operator ="";
+
+const currentDisplayNumber =  document.querySelector('currentNumber');
+const previousDisplayNumber =  document.querySelector('previousNumber');
+
+const equal = document.querySelector(".equal");
+
+const decimal = document.querySelector(".decimal");
+
+const clear = document.querySelector(".clear");
+
+const numberButtons = document.querySelectorAll(".number");
+
+const operators = document.querySelectorAll(".operator");
+
+
+numberButtons.forEach((btn) => {
+    btn.addEventListener("click",(e)=>{
+        handleNumber(e.target.textContent);
+    });
+});
+
+function handleNumber(number){
+    console.log(number);
+}
